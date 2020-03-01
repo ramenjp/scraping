0d1d289e24aba137e693e0045b10aa1bea766b9e
ramenjp <takagiyousuke5@gmail.com>
2020-03-01 18:59:20 +0900

    get article text

--- scraping.py
+++ scraping.py
@@ -40,13 +40,12 @@ for i in range(1,2):
     area_links = ["https:" + link['href'] for div in soup.find_all("div",class_="top-area-list-group-thum") for link in div.find_all('a')]
     print(area_links)
     print("--------------------------------------------------")
-    for link in area_links:
+    for favorite_spot_url in area_links:
         # 「エリアごとの人気スポット欄」のリストから１つずつ取り出して解析
-        favorite_spot_url = link
         r2 = requests.get(favorite_spot_url)
         soup2 = bs4(r2.content, "html.parser")
         # 人気スポットページの記事のURLをすべて取得
-        article_url_list = ["https:" + link['href'] for div in soup.find_all("div",class_="panel") for link in div.find_all('a')]
+        article_url_list = ["https:" + link['href'] for div in soup2.find_all("div",class_="panel") for link in div.find_all('a')]
         print("人気スポットのURL一覧")
         print(article_url_list)
         print("--------------------------------------------------")
@@ -56,4 +55,6 @@ for i in range(1,2):
             r3 = requests.get(article_link)
             soup3 = bs4(r3.content, "html.parser")
             article_title = soup3.find("h1" ,class_="article-title")
-            print(article_title.text)
+            print("【タイトル】"+article_title.text)
+            for article in soup3.find_all(class_="article__description"):
+             print(article.text)
