2eb958c5c091db9125059d5ec897c1ea64230a4f
ramenjp <takagiyousuke5@gmail.com>
2020-03-01 20:01:52 +0900

    ページごとのタイトルと記述まで

--- scraping.py
+++ scraping.py
@@ -43,30 +43,38 @@ for i in range(1,6):
         r2 = requests.get(favorite_spot_url)
         areas_soup = bs4(r2.content, "html.parser")
         # 人気スポットページの記事のURLをすべて取得
-        article_url_list = ["https:" + link['href'] for div in areas_soup.find_all("div",class_="btn-container",limit=1) for link in div.find_all('a')]
+        articles_url_list = ["https:" + link['href'] for div in areas_soup.find_all("div",class_="btn-container",limit=1) for link in div.find_all('a')]
         print("エリアの記事一覧url")
-        print(article_url_list)
-        r_article_list = requests.get(article_url_list[0])
+        print(articles_url_list)
+        r_article_list = requests.get(articles_url_list[0])
         article_list_soup = bs4(r_article_list.content, "html.parser")
         article_num_description = article_list_soup.find("p",class_="main-content__outline").text
+        print(article_num_description)
         article_num = article_num_description[article_num_description.find("記事が")+3:article_num_description.find("件")]
         print(article_num)
         if int(article_num) > 30:
             page_num = int(int(article_num)/30) + 1
             i = 2
             while i <= page_num:
-                article_url_list.append(article_url_list[0]+"pg"+str(i))
+                articles_url_list.append(articles_url_list[0]+"pg"+str(i))
                 i = i +1
-        print(article_url_list)
+        print(articles_url_list)
         print("-----------------------------------------------------------------------------------------------------")
-        # 記事からタイトルと内容をスクレイピング
-        for article_link in article_url_list:
-            r3 = requests.get(article_link)
-            soup3 = bs4(r3.content, "html.parser")
-            article_title = soup3.find("h1" ,class_="article-title")
-            print("【タイトル】"+article_title.text)
-            for article in soup3.find_all(class_=["article__description--head","article__description"]):
-             print(article.text)
-             # writer.writerow(area_name,prefecture_name,article_title.text,article.text)
-
-            print("-----------------------------------------------------------------------------------------------")
+        # 人気スポットページの記事のURLをすべて取得
+        for articles_url in articles_url_list:
+            r_article_url = requests.get(articles_url)
+            article_soup = bs4(r_article_url.content, "html.parser")
+            article_url_list = ["https:" + link['href'] for div in article_soup.find_all("div",class_="panel") for link in div.find_all('a')]
+            print("人気スポットのURL一覧")
+            print(article_url_list)
+            print("-----------------------------------------------------------------------------------------------------")
+            # 記事からタイトルと内容をスクレイピング
+            for article_link in article_url_list:
+                r3 = requests.get(article_link)
+                soup3 = bs4(r3.content, "html.parser")
+                article_title = soup3.find("h1" ,class_="article-title")
+                print("【タイトル】"+article_title.text)
+                for article in soup3.find_all(class_=["article__description--head","article__description"]):
+                    print(article.text)
+                    # writer.writerow(area_name,prefecture_name,article_title.text,article.text)
+                print("-----------------------------------------------------------------------------------------------")
